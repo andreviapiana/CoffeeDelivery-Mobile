b2969c54f0677d6eff9e5a182ebbdb1e650039cd
andreviapiana <andreviapiana@gmail.com>
2023-09-13 14:59:18 -0300

    ✨feat: Home - Background com Imagem de Grãos

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,20 @@
-import { Text, VStack } from 'native-base'
+import { HStack, Text, VStack } from 'native-base'
 import { THEME } from '@theme'
 
 import { HomeHeader } from '@components/HomeHeader'
 import { Search } from '@components/Search'
 
+import CoffeGrainSvg from '../../assets/coffee_grain.svg'
+
 export function Home() {
   return (
     <VStack flex={1} backgroundColor={THEME.colors.WHITE}>
-      <VStack height={342} pt={44} backgroundColor={THEME.colors.GRAY100}>
+      <VStack height={386} pt={44} backgroundColor={THEME.colors.GRAY100}>
         <HomeHeader />
         <Search />
+        <HStack marginRight={1} justifyContent={'flex-end'} marginTop={-5}>
+          <CoffeGrainSvg />
+        </HStack>
       </VStack>
       <Text color={THEME.colors.WHITE}>Home</Text>
     </VStack>
